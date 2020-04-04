0570d399b7424ec4407462ffa6337f923a6b9127
NickBerg <niccolo.berguglia@gmail.com>
2020-04-04 19:56:51 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String url = 'http://scuola.weably.net/posizioni.php';
+
+  Future<String> makeRequest() async {
+    var response = await http.post(
+      Uri.encodeFull(url),
+      headers: {"Accept": "text/html"},
+    );
+
+    print(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Center(
+            child: new RaisedButton(
+      child: new Text('Make Request'),
+      onPressed: makeRequest,
+    )));
+  }
+}
